6fd7e8077160114c232a4442870d1aadbb8c4e96
Sherzod <daminovsherzod2002@gmail.com>
2022-07-18 20:51:34 +0500

    read_data

--- read_data.py
+++ read_data.py
@@ -10,4 +10,8 @@ def read_data(file_path: str)->dict:
     
     """
     #open file
-    return
+    f = open(file_path, encoding="UTF8")
+    data = f.read()
+    x = json.loads(data)
+    return x
+
