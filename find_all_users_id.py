ae1011ba1d1fb9ddb5d0b23a4abd093de6bfb52f
Sherzod <daminovsherzod2002@gmail.com>
2022-07-21 09:10:55 +0500

    find_id

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -12,15 +12,14 @@ def find_all_users_id(data: dict)->list:
     id = []
     messages = data['messages']
     for i in messages:
-        x = i.get("actor_id",False)
+        # x = i.get("actor_id",False)
         y = i.get("from_id",False)
-        if x and x not in id:
-            id.append(x)
-        # if y and y not in id:
-        #     id.append(y) 
+        # if x and x not in id:
+        #     id.append(x)
+        if y and y not in id:
+            id.append(y) 
     # id.remove('channel1640165484')
     # id.remove('channel1474589327')       
     return id
-
 data = read_data('data/result.json')
 print(find_all_users_id(data))
